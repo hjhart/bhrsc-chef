58bfd2e191bc42ae4430ef2e8dd3cedf70754e64
James Hart <hjhart@gmail.com>
2014-06-25 00:35:55 -0700

    move stuff

--- site-cookbooks/phpapp/recipes/default.rb
+++ site-cookbooks/phpapp/recipes/default.rb
@@ -11,15 +11,3 @@
 include_recipe 'phpapp::nginx'
 include_recipe 'phpapp::php'
 
-directory "/var/run/php-fastcgi" do
-  recursive true
-  owner "www"
-  group "www"
-end
-
-template "/opt/local/bin/php-fastcgi" do
-  source "fast-cgi.erb"
-  mode 0755
-  owner "www"
-  group "www"
-end
