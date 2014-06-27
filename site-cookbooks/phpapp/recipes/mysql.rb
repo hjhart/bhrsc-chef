bbdd11dd3b341d4ba69c4fc2caa697fe757612fa
James Hart <hjhart@gmail.com>
2014-06-27 09:50:19 -0700

    Pipe the grant command to mysql :)

--- site-cookbooks/phpapp/recipes/mysql.rb
+++ site-cookbooks/phpapp/recipes/mysql.rb
@@ -12,4 +12,4 @@ execute %Q{ echo 'create database #{mysql_database}' | mysql -u root } do
   not_if %Q{ echo 'show databases' | mysql -u root | grep bhrsc }
 end
 
-execute %Q{ echo 'GRANT ALL PRIVILEGES ON *.* TO '#{mysql_user}'@'localhost' IDENTIFIED BY '#{mysql_password}' WITH GRANT OPTION; }
+execute %Q{ echo 'GRANT ALL PRIVILEGES ON *.* TO '#{mysql_user}'@'localhost' IDENTIFIED BY '#{mysql_password}' WITH GRANT OPTION; | mysql -u root }
