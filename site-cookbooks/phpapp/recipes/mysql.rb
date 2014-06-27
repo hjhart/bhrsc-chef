4df4cebf074645dea787065c8ce7947815aacc8a
James Hart <hjhart@gmail.com>
2014-06-27 09:45:55 -0700

    Do not create database if it already exists

--- site-cookbooks/phpapp/recipes/mysql.rb
+++ site-cookbooks/phpapp/recipes/mysql.rb
@@ -8,5 +8,8 @@ mysql_user = node['phpapp']['mysql']['user']
 mysql_database = node['phpapp']['mysql']['database']
 mysql_password = node['phpapp']['mysql']['password']
 
-execute %Q{ echo 'create database #{mysql_database}' | mysql -u root }
+execute %Q{ echo 'create database #{mysql_database}' | mysql -u root } do
+  not_if %Q{ echo 'show databases' | mysql -u root | grep bhrsc }
+end
+
 execute %Q{ echo 'GRANT ALL PRIVILEGES ON *.* TO '#{mysql_user}'@'localhost' IDENTIFIED BY '#{mysql_password}' WITH GRANT OPTION; }
